5cca8fbcedda89dd5a6de14438b78cb2b2420c77
Ubuntu <ubuntu@ip-172-31-25-26.ec2.internal>
2020-02-06 19:16:30 +0000

    added files for survey gen and inquiry sending

---
+++ listings_inquirer/handle_listing_page.py
@@ -0,0 +1,206 @@
+import random
+import datetime
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+
+from wait_and_get import wait_and_get
+
+NAME_CSS    = ["#name_PDP_TOP_THIRD", "#nameInput"]
+EMAIL_CSS   = ["#email_PDP_TOP_THIRD", "#emailInput"]
+PHONE_CSS   = ["#phone_PDP_TOP_THIRD","#phoneInput"]
+SEND_CSS    = ['#main-content > div.BasicPageLayout__BasicPageLayoutContainer-sc-7vcr4x-0.gMTacw > div.BasicPageLayout__BasicPageContent-sc-7vcr4x-1.lhjvlp > div.HomeDetailsContentOverview__ContentWithLeadFormGrid-sc-1lql7o5-0.bHYvJo.Grid__GridContainer-sc-5ig2n4-1.coXJy > div.Grid__CellBox-sc-5ig2n4-0.HomeDetailsContentOverview__HiddenExceptLargeCell-sc-1lql7o5-1.eNNXco > div > div > form > div > div:nth-child(2) > div > button', '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-sc-7vcr4x-0.gMTacw > div.BasicPageLayout__BasicPageContent-sc-7vcr4x-1.lhjvlp > div.HomeDetailsContentOverview__ContentWithLeadFormGrid-sc-1lql7o5-0.bHYvJo.Grid__GridContainer-sc-5ig2n4-1.coXJy > div.Grid__CellBox-sc-5ig2n4-0.HomeDetailsContentOverview__HiddenExceptLargeCell-sc-1lql7o5-1.ipOYVS > div > div > form > div > div:nth-child(2) > div > div > button']
+#MESSAGE_CSS = '#topPanelLeadForm > div > div > span > div > div.madlibsForm.form > div:nth-child(6) > a'
+#TEXTB_CSS   = '#textarea'
+
+def try_page_element(driver, element, element_text):
+	for text in element_text:
+		try:
+		 	check = driver.find_element_by_css_selector(element).text
+		 	if text in check:
+		 		print('Page Status: ' + check)
+		 		return 1
+		except NoSuchElementException:
+			return 0
+	return 0
+
+def send_message(driver,name, email, phone_num, address,url,send):
+	print('Address    : ' + address)
+	print('URL        :' + url)
+
+	try:
+		driver.get(url) # start the driver window
+		# sleep(100)
+		print('Page Title : ' + driver.title) # print the title of the page
+		if driver.title == 'Access to this page has been denied.':
+			print('Page Status: Blocked' + '\n' + str(address) + ': ' + str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+			return "RESTART DRIVER"
+		elif driver.title == 'Page Not Found | Trulia':
+			print('Page Status: 404 Error')
+			return 'SOLD'
+
+	except WebDriverException: # check for webdriver exception
+		print('WebDriverException: Restarting... ')
+		driver.quit()
+		return "RESTART DRIVER" # "NEED NEW ADDRESS"
+
+	# wait for the page to load the map on listing page
+	# status_cond   = EC.presence_of_element_located((By.CSS_SELECTOR,'#mapViewCard > div > div.cardDescription.pam > div > p.typeEmphasize.activeLink.mtn.ptn'))
+	# status_handle = wait_and_get(driver, status_cond, 10)
+
+	form_check = try_page_element(driver,SEND_CSS[0], ['Request Sent'])
+
+	if form_check > 0:
+		print('No Send Button')
+		return 'RESTART DRIVER'
+
+	error = 0
+
+	element = '#__next > div > section > div > div.HomeDetailsHero__Container-hubkl0-0.eFsGnT > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.eiQVQP > div:nth-child(1) > div > div.Text__TextContainerBase-s1cait9d-1.krEVrP.Text__TextBase-s1cait9d-0-div.lgawpF'
+	error  += try_page_element(driver, element,['Off Market','Recently Sold'])
+
+	element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-mfegza-0.fVMZGj > div.HomeDetailsHero__Container-hubkl0-0.bbKmZG > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.bHObIE > div.HomeDetailsHero__HomeStatusTitle-hubkl0-7.eLIcbC > span > span'
+	error  += try_page_element(driver,element,['OFF MARKET'])
+
+	element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-mfegza-0.fVMZGj > div.HomeDetailsHero__Container-hubkl0-0.bbKmZG > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.dodyUc > div.HomeDetailsHero__HomeStatusTitle-hubkl0-7.BFhlM > span > span'
+	error  += try_page_element(driver,element,['OFF MARKET'])
+
+        element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-mfegza-0.fVMZGj > div.HomeDetailsHero__Container-hubkl0-0.bbKmZG > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.dodyUc > div.HomeDetailsHero__HomeStatusTitle-hubkl0-6.isuzeT > span > span'
+        error  += try_page_element(driver,element,['OFF MARKET'])
+
+	element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-mfegza-0.fVMZGj > div.HomeDetailsHero__Container-hubkl0-0.bbKmZG > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.dodyUc > div.HomeDetailsHero__HomeStatusTitle-hubkl0-6.gZQsUf > span > span'
+	error  += try_page_element(driver,element,['OFF MARKET'])
+
+        element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-mfegza-0.fVMZGj > div.HomeDetailsHero__Container-hubkl0-0.dBslmq > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.dodyUc > div.HomeDetailsHero__HomeStatusTitle-hubkl0-6.gZQsUf > span'
+        error += try_page_element(driver,element,['OFF MARKET'])
+
+	element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-sc-7vcr4x-0.gMTacw > div.HomeDetailsHero__Container-hubkl0-0.dBslmq > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.dodyUc > div.HomeDetailsHero__HomeStatusTitle-hubkl0-6.gZQsUf > span > span'
+	error += try_page_element(driver,element,['OFF MARKET'])
+
+        element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-sc-7vcr4x-0.gMTacw > div.HomeDetailsHero__Container-hubkl0-0.jcSior > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.dodyUc > div.HomeDetailsHero__HomeStatusTitle-hubkl0-6.gZQsUf > span > span'
+        error += try_page_element(driver,element,['OFF MARKET'])
+
+	element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-mfegza-0.fVMZGj > div.HomeDetailsHero__Container-hubkl0-0.bbKmZG > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.bHObIE > div.HomeDetailsHero__HomeStatusTitle-hubkl0-7.eLIcbC > span.PropertyTag-sc-5t90lx-0.ebIvTY.Tag__TagBase-sc-1rp6fz0-1.clFGBQ.Text__TextBase-sc-1cait9d-0.dJEjin > span'
+	error  += try_page_element(driver,element,['SOLD'])
+
+	element = '#__next > div > section > div.BasicPageLayout__BasicPageLayoutContainer-mfegza-0.fVMZGj > div.HomeDetailsHero__Container-hubkl0-0.bOZPCt > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-5.cUoBll > div.HomeDetailsHero__HomeStatusTitle-hubkl0-8.jNtMuF > span > span'
+	error  += try_page_element(driver,element,['SOLD'])
+
+        element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-mfegza-0.fVMZGj > div.HomeDetailsHero__Container-hubkl0-0.dBslmq > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.dodyUc > div.HomeDetailsHero__HomeStatusTitle-hubkl0-6.gZQsUf > span.PropertyTag-sc-5t90lx-0.iLjQlr.Tag__TagBase-sc-1rp6fz0-1.clFGBQ.Text__TextBase-sc-1cait9d-0.dJEjin'
+        error  += try_page_element(driver,element,['SOLD'])
+        
+        element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-mfegza-0.fVMZGj > div.HomeDetailsHero__Container-hubkl0-0.dBslmq > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.dodyUc > div.HomeDetailsHero__HomeStatusTitle-hubkl0-6.gZQsUf > span.PropertyTag-sc-5t90lx-0.kHqVgt.Tag__TagBase-sc-1rp6fz0-1.clFGBQ.Text__TextBase-sc-1cait9d-0.dJEjin'
+        error  += try_page_element(driver,element,['SOLD'])
+
+        element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-sc-7vcr4x-0.gMTacw > div.HomeDetailsHero__Container-hubkl0-0.dBslmq > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.dodyUc > div.HomeDetailsHero__HomeStatusTitle-hubkl0-6.gZQsUf > span.PropertyTag-sc-1oqnx2r-0.euRTRd.Tag__TagBase-l0an8r-1.iEnRVM.Text__TextBase-sc-1i9uasc-0.dVmMGh > span'
+        error  += try_page_element(driver,element,['SOLD'])
+
+        element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-sc-7vcr4x-0.gMTacw > div.HomeDetailsHero__Container-hubkl0-0.jcSior > div > div > div.HomeDetailsHero__HomeInfoBanner-hubkl0-4.dodyUc > div.HomeDetailsHero__HomeStatusTitle-hubkl0-6.gZQsUf > span.PropertyTag-sc-1oqnx2r-0.euRTRd.Tag__TagBase-l0an8r-1.iEnRVM.Text__TextBase-sc-1i9uasc-0.dVmMGh > span'
+        error += try_page_element(driver,element,['SOLD'])
+
+	if error > 0:
+		return 'SOLD'
+
+	# if status_handle == 0:
+	# 	print('Page Could Not load')
+	# 	return "RESTART DRIVER"
+
+	element = '#marketStatusLabel'
+	error  += try_page_element(driver,element, ['OFF MARKET','RECENTLY SOLD'])
+
+	element = '#propertySummary > div > div > div.xsCol24.smlCol24.mdCol8.lrgCol8.pan.ptxsXxsVisible.ptlSmlVisible > div > div:nth-child(1) > div > span'
+	error  += try_page_element(driver,element, ['PENDING'])
+
+	element = 'div.mdCol8:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2)'
+	error  += try_page_element(driver,element, ['INCOME RESTRICTED'])
+
+	element = 'span.typeCaps:nth-child(1)'
+	error  += try_page_element(driver,element, ['FOR SALE','FORECLOSURE'])
+
+	element = 'div.pvl:nth-child(3) > button:nth-child(1)'
+	error  += try_page_element(driver,element, ['Notify Me When Available'])
+
+	element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-sc-7vcr4x-0.gMTacw > div.BasicPageLayout__BasicPageContent-sc-7vcr4x-1.lhjvlp > div.HomeDetailsContentOverview__ContentWithLeadFormGrid-sc-1lql7o5-0.bHYvJo.Grid__GridContainer-sc-5ig2n4-1.coXJy > div.Grid__CellBox-sc-5ig2n4-0.HomeDetailsContentOverview__HiddenExceptLargeCell-sc-1lql7o5-1.ipOYVS > div > div > button'
+	error  += try_page_element(driver,element, ['Notify Me When Available'])
+
+	element = '#propertySummary > div > div.row.man.positionRelative.summaryDetails > div.xsCol24.smlCol24.mdCol8.lrgCol8 > div > div.typeLowlight.typeEmphasize.mvn.h6.miniHidden.xxsHidden > span:nth-child(2)'
+	error  += try_page_element(driver,element, ['SENIOR HOUSING'])
+
+        element = '#main-content > div.BasicPageLayout__BasicPageLayoutContainer-sc-7vcr4x-0.gMTacw > div.BasicPageLayout__BasicPageContent-sc-7vcr4x-1.lhjvlp > div.HomeDetailsContentOverview__ContentWithLeadFormGrid-sc-1lql7o5-0.bHYvJo.Grid__GridContainer-sc-5ig2n4-1.coXJy > div.Grid__CellBox-sc-5ig2n4-0.HomeDetailsContentOverview__HiddenExceptLargeCell-sc-1lql7o5-1.ipOYVS > div > div > div.Grid__CellBox-sc-5ig2n4-0.fHUtjJ > button'
+        error  += try_page_element(driver,element, ['Request Viewing'])
+
+	if error > 0:
+		return 'SOLD'
+
+	print('Page Status: Available')
+        
+        try:
+	    page_structure = 0
+	    for i in range(len(NAME_CSS)):
+		name_cond   = EC.presence_of_element_located((By.CSS_SELECTOR,NAME_CSS[i]))
+		name_handle = wait_and_get(driver, name_cond, 10)
+		if name_handle != 0:
+			page_structure = i
+			break
+
+	    if name_handle == 0:
+                try:
+                    notify_text = driver.find_element_by_css_selector('#main-content > div.BasicPageLayout__BasicPageLayoutContainer-sc-7vcr4x-0.gMTacw > div.BasicPageLayout__BasicPageContent-sc-7vcr4x-1.lhjvlp > div.HomeDetailsContentOverview__ContentWithLeadFormGrid-sc-1lql7o5-0.bHYvJo.Grid__GridContainer-sc-5ig2n4-1.coXJy > div.Grid__CellBox-sc-5ig2n4-0.HomeDetailsContentOverview__HiddenExceptLargeCell-sc-1lql7o5-1.eNNXco > div > div > button')
+                    print("Notify Text")
+                except:
+			try:
+				income_restriction = driver.find_element_by_css_selector('#main-content > div.BasicPageLayout__BasicPageLayoutContainer-sc-7vcr4x-0.gMTacw > div.BasicPageLayout__BasicPageContent-sc-7vcr4x-1.lhjvlp > div.HomeDetailsContentOverview__ContentWithLeadFormGrid-sc-1lql7o5-0.bHYvJo.Grid__GridContainer-sc-5ig2n4-1.coXJy > div.Grid__CellBox-sc-5ig2n4-0.HomeDetailsContentOverview__HiddenExceptLargeCell-sc-1lql7o5-1.eNNXco > div > div > div.Grid__CellBox-sc-5ig2n4-0.fxeKaR > button')
+				print("Income Restriction")
+			except:
+		    		print('Name Cond: No Such Element')
+		    		return 'RESTART DRIVER'
+	
+	    print("Page structure: " + str(page_structure))
+
+	    name_handle.send_keys(name) # once it is found, send the name string to it
+
+	    # send the email string
+	    email_handle = driver.find_element_by_css_selector(EMAIL_CSS[page_structure])
+	    email_handle.send_keys((str(email) + '@gmail.com'))
+	    # send the phone string
+	    phone_handle = driver.find_element_by_css_selector(PHONE_CSS[page_structure])
+	    phone_handle.send_keys(str(phone_num))
+
+	    print("Email: {}@gmail.com\nPhone: {}".format(str(email), str(phone_num)))
+            for i in range(len(SEND_CSS)):
+                try:
+	            send_handle = driver.find_element_by_css_selector(SEND_CSS[i])
+                    break
+                except:
+                    print("Button not found at index {}".format(i))
+
+        except:
+            try:
+                notify_text = driver.find_element_by_css_selector('#main-content > div.BasicPageLayout__BasicPageLayoutContainer-sc-7vcr4x-0.gMTacw > div.BasicPageLayout__BasicPageContent-sc-7vcr4x-1.lhjvlp > div.HomeDetailsContentOverview__ContentWithLeadFormGrid-sc-1lql7o5-0.bHYvJo.Grid__GridContainer-sc-5ig2n4-1.coXJy > div.Grid__CellBox-sc-5ig2n4-0.HomeDetailsContentOverview__HiddenExceptLargeCell-sc-1lql7o5-1.eNNXco > div > div > button')
+                #if 'notify me' in notify_text.lower():
+                send = 0
+                print("Notify Me When Available Button")
+
+            except:
+		try:
+			income_restriction = driver.find_element_by_css_selector('#main-content > div.BasicPageLayout__BasicPageLayoutContainer-sc-7vcr4x-0.gMTacw > div.BasicPageLayout__BasicPageContent-sc-7vcr4x-1.lhjvlp > div.HomeDetailsContentOverview__ContentWithLeadFormGrid-sc-1lql7o5-0.bHYvJo.Grid__GridContainer-sc-5ig2n4-1.coXJy > div.Grid__CellBox-sc-5ig2n4-0.HomeDetailsContentOverview__HiddenExceptLargeCell-sc-1lql7o5-1.eNNXco > div > div > div.Grid__CellBox-sc-5ig2n4-0.fxeKaR > button')
+                       	print("Income Restriction Button")
+			send = 0
+		except:
+                	print("Something Went Wrong")
+                	return 'RESTART DRIVER'
+
+	if send == 1:
+		print('Clicking...')
+		driver.save_screenshot("/home/ubuntu/trulia/scripts/confirmation.png")
+		send_handle.click()
+
+		time_sent = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+		print('Send Status: Sent')
+		sleep(random.randint(10,20))
+		return time_sent
+	else:
+		print('Send Status: Not Sent')
+		return ''
---
+++ survey_gen/survey.py
@@ -0,0 +1,122 @@
+import xlwt
+import xlrd
+from xlutils.copy import copy
+import pandas as pd
+import datetime
+import os
+from tqdm import tqdm
+
+print()
+
+# delete old survey files if necessary
+output_files = [f for f in os.listdir('output') if not f.startswith('.')]
+if output_files:
+	for output_file in tqdm(output_files, desc="Deleting Old Survey Files", bar_format="{l_bar}{bar}|  {n_fmt}/{total_fmt}   "):
+		os.remove(os.getcwd() + '/output/' + output_file)
+	print()
+
+# find input files
+TIMESTAMP_FILE = None
+TEMPLATE_FILE = None
+input_files = [f for f in os.listdir('input') if not f.startswith('.')]
+for input_file in input_files:
+	if ".csv" in input_file:
+		TIMESTAMP_FILE = input_file
+	if ".xls" in input_file:
+		TEMPLATE_FILE = input_file
+
+names = [
+		['Jalen','Jackson'],
+		['Lamar','Williams'],
+		['DaQuan','Robinson'],
+		['Nia','Harris'],
+		['Ebony','James'],
+		['Shanice','Thomas'],
+		['Caleb','Peterson'],
+		['Charlie','Myers'],
+		['Ronnie','Miller'],
+		['Aubrey','Murphy'],
+		['Erica','Cox'],
+		['Leslie','Wood'],
+		['Jorge','Rodriguez'],
+		['Pedro','Sanchez'],
+		['Luis','Torres'],
+		['Isabella','Lopez'],
+		['Mariana','Morales'],
+		['Jimena','Ramirez']
+	]
+
+currentDate = datetime.date.today()
+month, day, year = str(currentDate.month), str(currentDate.day), str(currentDate.year)
+
+df = pd.read_csv(os.getcwd() + '/input/' + TIMESTAMP_FILE)
+cols = df.columns.tolist()
+new_cols = cols[:9]
+num_addresses = int(cols[-1].split(' ')[-1])
+for i in tqdm(range(1, num_addresses + 1), desc="Reading Timestamps File", bar_format="{l_bar}{bar}|  {n_fmt}/{total_fmt}   "):
+	for first,last in names:
+		#print(first, last)
+		if str(df.loc[df['first name'] == first,'address ' + str(i)].values[0]).lower() == 'nan':
+			df.loc[df['first name'] == first,'address ' + str(i)] = None
+		else: 
+			#df.loc[df['first name'] == first,'address ' + str(i)] = df.loc[df['first name'] == first,'address ' + str(i)].values[0].split(',')[0].split('(')[1]
+                        loc = df.loc[df['first name'] == first,'address ' + str(i)]
+                        if(len(loc.values[0].split(',')) == 2):
+                            df.loc[df['first name'] == first,'address ' + str(i)] = df.loc[df['first name'] == first,'address ' + str(i)].values[0].split(',')[0].split('(')[1]
+                        else:
+                            df.loc[df['first name'] == first,'address ' + str(i)] = "(" + df.loc[df['first name'] == first,'address ' + str(i)].values[0].split(',')[0][1:-3] + ") " + df.loc[df['first name'] == first,'address ' + str(i)].values[0].split(',')[1]
+
+	new_cols.append('address ' + str(i))
+df = df[new_cols]
+
+file = xlrd.open_workbook(os.getcwd() + '/input/' + TEMPLATE_FILE)
+print()
+
+for name in tqdm(names, desc="Writing New Survey Files", bar_format="{l_bar}{bar}|  {n_fmt}/{total_fmt}   "):
+	myFile = copy(file)
+	# get list of addresses for a specific person
+	df_upper = df.loc[(df['first name'] == name[0]) & (df['last name'] == name[1])]
+	df_upper = df_upper.reset_index(drop=True)
+	df_upper = df_upper[df_upper.columns.tolist()[9:]]
+	addresses = list(set(df_upper.iloc[0,:]))
+	addresses = [x for x in addresses if (str(x) != 'nan' and x != None)]
+
+	# create dataframe that is basically the first half of the 2nd sheet of the xls sheet
+	df_upper = pd.DataFrame(index=range(1+len(addresses)), columns=['list name', 'name', 'label'])
+	df_upper['list name'][0] = 'people_name'
+	df_upper['name'][0] = name[0] + ' ' + name[1]
+	df_upper['label'][0] = name[0] + ' ' + name[1]
+	for i in range(len(addresses)):
+		df_upper['list name'][i+1] = 'address_name'
+		df_upper['name'][i+1] = addresses[i]
+		df_upper['label'][i+1] = addresses[i]
+
+	# create dataframe that is basically the second half of the 2nd sheet of the xls sheet
+	sheet_2 = file.sheet_by_index(1)
+	num_rows = sheet_2.nrows - 1 # nrows includes header row
+	df_lower = pd.DataFrame(index=range(num_rows), columns=['list name', 'name', 'label'])
+	for i in range(num_rows):
+		df_lower['list name'][i] = sheet_2.row(i+1)[0].value
+		df_lower['name'][i] = sheet_2.row(i+1)[1].value
+		df_lower['label'][i] = sheet_2.row(i+1)[2].value
+
+	# merge the dataframes together
+	df_final = df_upper.append(df_lower, ignore_index=True)
+
+	# overwrite the 2nd sheet with new name and addresses
+	s = myFile.get_sheet(1)
+	for i in range(len(df_final)):
+		s.write(i+1, 0, df_final['list name'][i])
+		s.write(i+1, 1, df_final['name'][i])
+		s.write(i+1, 2, df_final['label'][i])
+	
+	# overwrite values in 3rd sheet
+	s = myFile.get_sheet(2)
+	s.write(1, 0, name[0] + '_' + name[1] + '_survey')		# title
+	s.write(1, 1, name[0] + '_' + name[1] + '_survey_1_1_5')	# id string
+
+	fileName = name[0]+'-'+name[1]+'_'+month+'_'+day+'_'+year+'.xls'
+
+	myFile.save(os.getcwd() + '/output/' + fileName)
+
+print()
